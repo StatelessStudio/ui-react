788925468d28e7b1a8a43138db5f62478a8fe45c
Drew Immerman <drew@stateless.studio>
2026-05-09 15:08:07 -0400

    ui: button focus colors

--- src/colors/colors.ts
+++ src/colors/colors.ts
@@ -26,25 +26,25 @@ export const textColors = {
 } as const satisfies Record<ColorVariant & 'default', string>;
 
 export const focusRingColors = {
-	primary: 'focus:ring-primary',
-	secondary: 'focus:ring-secondary',
-	accent: 'focus:ring-accent',
-	muted: 'focus:ring-muted',
-	success: 'focus:ring-success',
-	warning: 'focus:ring-warning',
-	danger: 'focus:ring-danger',
-	info: 'focus:ring-info',
-	gradient: 'focus:ring-primary',
+	primary: 'focus:ring-primary/70',
+	secondary: 'focus:ring-secondary/70',
+	accent: 'focus:ring-accent/70',
+	muted: 'focus:ring-muted/70',
+	success: 'focus:ring-success/70',
+	warning: 'focus:ring-warning/70',
+	danger: 'focus:ring-danger/70',
+	info: 'focus:ring-info/70',
+	gradient: 'focus:ring-primary/70',
 } as const satisfies Record<ColorVariant, string>;
 
 export const hasFocusVisibleColors = {
-	primary: 'has-[:focus-visible]:ring-primary',
-	secondary: 'has-[:focus-visible]:ring-secondary',
-	accent: 'has-[:focus-visible]:ring-accent',
-	muted: 'has-[:focus-visible]:ring-muted-foreground',
-	success: 'has-[:focus-visible]:ring-success',
-	warning: 'has-[:focus-visible]:ring-warning',
-	danger: 'has-[:focus-visible]:ring-danger',
-	info: 'has-[:focus-visible]:ring-info',
-	gradient: 'has-[:focus-visible]:ring-primary',
+	primary: 'has-[:focus-visible]:ring-primary/70',
+	secondary: 'has-[:focus-visible]:ring-secondary/70',
+	accent: 'has-[:focus-visible]:ring-accent/70',
+	muted: 'has-[:focus-visible]:ring-muted-foreground/70',
+	success: 'has-[:focus-visible]:ring-success/70',
+	warning: 'has-[:focus-visible]:ring-warning/70',
+	danger: 'has-[:focus-visible]:ring-danger/70',
+	info: 'has-[:focus-visible]:ring-info/70',
+	gradient: 'has-[:focus-visible]:ring-primary/70',
 } as const satisfies Record<ColorVariant, string>;
