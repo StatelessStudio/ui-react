158e57e5f58f904a7e0095603d202663c01a7c36
Drew Immerman <drew@stateless.studio>
2026-05-09 14:51:42 -0400

    fix: navigation component export

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ export * from './buttons';
 export * from './cards';
 export * from './feedback';
 export * from './layout';
+export * from './navigation';
 export * from './progress';
 export * from './typography';
