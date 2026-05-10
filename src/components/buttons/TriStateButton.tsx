0dba6322fa2648e948df13a71aae9e9eb7244f9c
Drew Immerman <drewimmerman@gmail.com>
2026-05-10 05:33:14 -0400

    feat: tri-state button component

---
+++ src/components/buttons/TriStateButton.tsx
@@ -0,0 +1,58 @@
+import { ReactNode } from 'react';
+import { ColorVariant } from '@/colors';
+import { cn } from '@/style-engine';
+import { Button, ButtonProps } from './Button';
+import { TriState } from '../progress';
+import { animate } from '@/animations';
+
+const animation = animate({
+	transitionColors: true,
+	duration: 'short',
+});
+
+export function TriStateButton({
+	children,
+	state,
+	size = 'md',
+	className = '',
+	color = 'primary',
+	...props
+}: {
+	children?: ReactNode;
+	state: null | 'loading' | 'success' | 'error';
+	size?: 'sm' | 'md' | 'lg';
+	className?: string;
+	color?: ColorVariant;
+} & ButtonProps<'button'>) {
+	const getButtonColor = () => {
+		if (state === 'loading') {
+			return color;
+		}
+		if (state === 'success') {
+			return 'success';
+		}
+		if (state === 'error') {
+			return 'danger';
+		}
+		return color;
+	};
+
+	return (
+		<Button
+			color={getButtonColor()}
+			className={cn(animation, 'disabled:opacity-90', className)}
+			size={size}
+			disabled={state !== null}
+			{...props}
+		>
+			{state === null && children}
+			{state !== null && (
+				<TriState
+					state={state}
+					size={size}
+					className={cn('text-white', className)}
+				/>
+			)}
+		</Button>
+	);
+}
--- src/components/buttons/index.ts
+++ src/components/buttons/index.ts
@@ -1,3 +1,4 @@
 export * from './Button';
 export * from './ButtonGroup';
 export * from './ToggleGroup';
+export * from './TriStateButton';
